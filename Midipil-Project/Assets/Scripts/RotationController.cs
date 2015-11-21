9057513b654e99fdbbafb5b7f3114b7aa5d30522
Aurelie Beauprez <aurelie.beauprez@gmail.com>
2015-11-21 05:45:57 +0100

    [NAVIGATION] * Fix rotation / move + Add Drift

---
+++ Midipil-Project/Assets/Scripts/RotationController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationController : MonoBehaviour {
+
+    private float boost = 1.0f;
+	private float moveSpeed = 1.0f;
+    private float rotationSpeed = 3.0f;
+
+	// Update is called once per frame
+	void Update () {
+
+        // LEFT STICK : MOVE FORWARD / BACKWARD & ROTATE 		
+		transform.Rotate(new Vector3(boost * moveSpeed * Input.GetAxis("Vertical"), rotationSpeed * Input.GetAxis("Horizontal"), 0));
+
+        // RIGHT STICK : DRIFT LEFT / RIGHT
+        transform.Rotate(new Vector3( 0f, 0f, Input.GetAxis("Right_Horizontal")));
+
+        // BOOSTER
+        //Input.GetButton("")
+
+    }
+}
