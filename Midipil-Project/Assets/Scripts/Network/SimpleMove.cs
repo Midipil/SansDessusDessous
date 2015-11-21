5e75953b3db3164f9fb0b5aa963f29ef551ee713
Aurelie Beauprez <aurelie.beauprez@gmail.com>
2015-11-21 21:48:44 +0100

    [NETWORK] + Start proto send / receive movement

---
+++ Midipil-Project/Assets/Scripts/Network/SimpleMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMove : MonoBehaviour {
+
+    private float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(- Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(- Vector3.right * speed * Time.deltaTime);
+        }
+
+    }
+}
