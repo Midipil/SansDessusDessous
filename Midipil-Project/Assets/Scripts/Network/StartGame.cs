a7f6df23228c6e0e9a2c5ede1b21b1cc4f9adc0b
Aurelie Beauprez <aurelie.beauprez@gmail.com>
2015-11-21 18:31:44 +0100

    [NETWOTK] Start network proto : Host/Client connexion with Oculus/Screenview

---
+++ Midipil-Project/Assets/Scripts/Network/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject serverView;
+    public GameObject clientView;
+
+    // Use this for initialization
+    void Start()
+    {
+        serverView.SetActive(false);
+        clientView.SetActive(false);
+
+        if (Network.isServer)
+        {
+            Debug.LogError("LOAD SERVER SCENE !");
+            serverView.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("LOAD CLIENT SCENE !");
+            clientView.SetActive(true);
+        }
+    }
+}
