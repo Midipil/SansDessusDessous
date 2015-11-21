6e5ef25c54d64aa2682bb941c3034f6e7c75d095
Baptiste Moizard <baptiste-moizard@orange.fr>
2015-11-21 12:46:32 +0100

    Laser beam

---
+++ Midipil-Project/Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeam : MonoBehaviour 
+{
+	LineRenderer line;
+
+	public float rayLength = 100;
+	public float minRadiusRayCast = 0;
+	
+	void Start (){
+		line = gameObject.GetComponent<LineRenderer>();
+	}
+
+	void Update (){
+
+		Ray ray = new Ray(transform.position + minRadiusRayCast * transform.forward, transform.forward);
+		RaycastHit hit;
+		
+		line.SetPosition(0, transform.position);
+		
+		if(Physics.Raycast(ray, out hit, rayLength))
+		{
+			line.SetPosition(1, hit.point);
+			if(hit.transform.tag == "Player")
+			{
+				Debug.Log ("Aie !");
+			}
+		}
+		else
+			line.SetPosition(1, ray.GetPoint(rayLength));
+		
+	}
+}
