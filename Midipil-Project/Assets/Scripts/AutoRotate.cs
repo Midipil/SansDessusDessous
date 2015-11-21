1cf6a70b266030de034ed2e30bbbfaa6d52f22a9
Baptiste Moizard <baptiste-moizard@orange.fr>
2015-11-21 01:52:21 +0100

    Added rotating sphere

---
+++ Midipil-Project/Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.left * speed * Time.deltaTime);
+	}
+}
