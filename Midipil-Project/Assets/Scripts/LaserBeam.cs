001a5c784a972f87d2e70a69eac43d7fa062b047
Aurelie Beauprez <aurelie.beauprez@gmail.com>
2015-11-22 09:10:22 +0100

    + Add Laser Beam 3s before die

--- Midipil-Project/Assets/Scripts/LaserBeam.cs
+++ Midipil-Project/Assets/Scripts/LaserBeam.cs
@@ -1,39 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LaserBeam : MonoBehaviour 
+public class LaserBeam : MonoBehaviour
 {
-	LineRenderer line;
+    LineRenderer line;
 
-	public float rayLength = 100;
-	public float minRadiusRayCast = 0;
-	public float laserDamages = 1;
-	public GameObject impactFX;
-	
-	void Start (){
-		line = gameObject.GetComponent<LineRenderer>();
-	}
+    public float rayLength = 100;
+    public float minRadiusRayCast = 0;
+    public float laserDamages = 1;
+    public GameObject impactFX;
+    public static int timeToKill = 3000;
+    public static int currentTimeHit = timeToKill;
+    private static System.Timers.Timer timerToKill;
 
-	void Update (){
 
-		Ray ray = new Ray(transform.position + minRadiusRayCast * transform.forward, transform.forward);
-		RaycastHit hit;
-		
-		line.SetPosition(0, transform.position);
-		
-		if(Physics.Raycast(ray, out hit, rayLength))
-		{
-			line.SetPosition(1, hit.point);
-			if(hit.transform.tag == "Player")
-			{
-				hit.transform.GetComponent<Player>().life -= laserDamages;
-				impactFX.transform.position = hit.point;
-				impactFX.SetActive(true);
-			}
-		}
-		else {
-			line.SetPosition(1, ray.GetPoint(rayLength));
-			impactFX.SetActive(false);
-		}
-	}
+    void Start()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+    }
+
+    public static void TimerKill()
+    {
+        timerToKill = new System.Timers.Timer(timeToKill);
+        timerToKill.Enabled = true;
+        timerToKill.AutoReset = true;
+        timerToKill.Start();
+        timerToKill.Elapsed +=
+            (object sender, System.Timers.ElapsedEventArgs e) => currentTimeHit -= 1000;
+    }
+
+
+    void Update()
+    {
+
+        if (currentTimeHit <= 0)
+            Debug.Log("You died!");
+
+        Ray ray = new Ray(transform.position + minRadiusRayCast * transform.forward, transform.forward);
+        RaycastHit hit;
+
+        line.SetPosition(0, transform.position);
+
+        if (Physics.Raycast(ray, out hit, rayLength))
+        {
+            line.SetPosition(1, hit.point);
+            if (hit.transform.tag == "Player")
+            {
+                //hit.transform.GetComponent<Player>().life -= laserDamages;
+                TimerKill();
+                impactFX.transform.position = hit.point;
+                impactFX.SetActive(true);
+            }
+            else
+            {
+                timerToKill.Stop();
+            }
+        }
+        else
+        {
+            line.SetPosition(1, ray.GetPoint(rayLength));
+            impactFX.SetActive(false);
+        }
+    }
 }
